1605a7ded99318f2eab9d4931e52d73ad1d2f3f9
Abolfazl <iabolfazl83@yahoo.com>
2026-09-05 01:06:01 +0330

    refactor: extract SearchBar as sibling of JobsList instead of nested child.

--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -3,7 +3,6 @@
 import {useSearchStore} from "@/lib/store";
 import {Job} from "@/lib/types";
 import JobListItem from "@/components/JobListItem";
-import SearchBar from "@/components/SearchBar";
 
 export default function JobsList({jobs}: { jobs: Job[] }) {
     const searchTerm = useSearchStore((state) => state.searchTerm);
@@ -12,17 +11,12 @@ export default function JobsList({jobs}: { jobs: Job[] }) {
         job.company.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
 
     return (
-        <div className="p-4 flex flex-col gap-4">
-            <div>
-                <SearchBar/>
-            </div>
-            <div>
-                <ul className="flex flex-col gap-2">
-                    {filteredJobs.map((job) => (
-                        <JobListItem job={job} key={job.id}/>
-                    ))}
-                </ul>
-            </div>
+        <div>
+            <ul className="flex flex-col gap-2">
+                {filteredJobs.map((job) => (
+                    <JobListItem job={job} key={job.id}/>
+                ))}
+            </ul>
         </div>
     );
 }
